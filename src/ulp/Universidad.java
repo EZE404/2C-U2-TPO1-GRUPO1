e72316ecf702828f56223aa9b86e94b8b2c656dc
Franco Ezequiel Albornoz <franco.ezequiel@outlook.com>
2020-10-21 18:56:27 -0300

    AlumnoData funcionando. Necesaria revisión ¿Hay que agregarle algún tipo de control a los métodos? Juzgar también los catch.

--- src/ulp/Universidad.java
+++ src/ulp/Universidad.java
@@ -11,9 +11,20 @@ public class Universidad {
         // TODO code application logic here
         Conexion conexion = new Conexion();
         AlumnoData alumno_data = new AlumnoData(conexion);
+        Alumno eze = new Alumno("Ezequiel", "Albornoz", "36227970", LocalDate.of(1991, 9, 16), true);
+        alumno_data.guardar_alumno(eze);
+        alumno_data.guardar_alumno(eze);
+        System.out.println(alumno_data.buscar_alumno(eze.getId_alumno()).toString());
+        alumno_data.borrar_alumno(100);
         
-        alumno_data.guardar_alumno(new Alumno("Ezequiel", "Albornoz", "36227970", LocalDate.of(1991, 9, 16), true));
+        Alumno franco = new Alumno(1, "Franco", "Ezequiel", "35789545", LocalDate.of(1990,7,12), false);
+        alumno_data.modificar_alumno(franco);
+        System.out.println(alumno_data.buscar_alumno(1).toString());
         
-    }
+        for (Alumno a: alumno_data.ver_alumnos()) {
+            System.out.println(a.toString());
+        }
+            
+        }
 
 }
